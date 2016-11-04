c88201c8216bfb7a970a46643e92c65caff59c7c
xiaomoxifeng <312770787@qq.com>
2016-11-04 19:02:08 +0800

    5

---
+++ chapter5/search1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+import string
+from chapter3.Downloader import Downloader
+from chapter3.MongoCache import MongoCache
+
+FIELDS = (
+'area', 'population', 'iso', 'country', 'capital', 'continent', 'tld', 'currency_code', 'currency_name', 'phone',
+'postal_code_format', 'postal_code_regex', 'languages', 'neighbours')
+
+
+def main():
+    template_url = 'http://example.webscraping.com/ajax/search.json?page={}&page_size=10&search_term={}'
+    countries = set()
+    download = Downloader(MongoCache())
+    for letter in string.lowercase:
+        page = 0
+        while True:
+            html = download(template_url.format(page, letter))
+            try:
+                ajax = json.loads(html)
+            except ValueError as e:
+                print e
+                ajax = None
+            else:
+                for record in ajax['records']:
+                    countries.add(record['country'])
+            page += 1
+            if ajax is None or page >= ajax['num_pages']:
+                break
+    open('countries.txt', 'w').write('\n'.join(sorted(countries)))
+
+
+if __name__ == '__main__':
+    main()
