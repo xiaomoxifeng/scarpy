855e9f1f00dc34f1073985f77bbeca37603e7b8c
xiaomoxifeng <312770787@qq.com>
2016-11-04 22:33:46 +0800

    6

--- chapter5/webkit_render.py
+++ chapter5/webkit_render.py
@@ -5,11 +5,12 @@ def main():
     driver = webdriver.Chrome()
     driver.get('http://example.webscraping.com/search')
     driver.find_element_by_id('search_term').send_keys('.')
-    driver.execute_script("document.getElementById('page_size').options[1].text = '1000'");
+    driver.execute_script("document.getElementById('page_size').options[1].text='1000'")
+
     driver.find_element_by_id('search').click()
     driver.implicitly_wait(30)
     links = driver.find_elements_by_css_selector('#results a')
-    countries = [link.text for link in links]
+    countries = [link for link in links]
     driver.close()
     print countries
 
